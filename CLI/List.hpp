5e868f613e5a8b8fc3b979f759638bd2837b87b1
Geoffrey Lentner <glentner@nd.edu>
2015-11-30 09:23:15 -0500

    fixed definition for conversions from CLI::List
    The previous definition didn't actually provide any conversion away from the
    original std::vector<std::string>.
    
    Now there is a fully generic template definition to convert the `value` member
    to any container of numeric types from the STL given `push_back()` is defined.
    
    If you wish to keep the elements as std::string or similar (const char*) use only
    a std::vector or std::list. Specializations have been defined for these conversions;
    the generic definition would otherwise lose information for std::string`s that contain
    multiple words.

--- CLI/List.hpp
+++ CLI/List.hpp
@@ -13,6 +13,10 @@
 
 #include <string>
 #include <sstream>
+#include <vector>
+#include <list>
+#include <typeinfo>
+
 #include "Argument.hpp"
 #include "IO.hpp"
 
@@ -32,8 +36,16 @@ public:
         virtual std::string GetHelp(const int& width);
 
         // conversion to other containers
-        template<typename ValueType>
-        operator std::vector<ValueType>();
+        template<typename ContainerType>
+        operator ContainerType() const;
+
+        // specializations for std::string, const char*, char*
+        operator std::vector<std::string>() const;
+        operator std::list<std::string>() const;
+        operator std::vector<char*>() const;
+        operator std::list<char*>() const;
+        operator std::vector<const char*>() const;
+        operator std::list<const char*>() const;
 
 };
 
@@ -50,11 +62,87 @@ inline std::string List::GetHelp(const int& width)
 }
 
 
-template<typename ValueType>
-inline List::operator std::vector<ValueType>()
+template<typename ContainerType>
+inline List::operator ContainerType() const
 {
-        return std::vector<ValueType>(value.begin(), value.end());
+        ContainerType return_value;
+
+        for (std::size_t i = 0; i < value.size(); i++)
+        {
+                std::stringstream convert(value[i]);
+                typename ContainerType::value_type interm_value;
+
+                if (!(convert >> interm_value))
+                throw Error("Cannot convert to type ", typeid(ContainerType::value_type).name(),
+                        "from std::string for ", typeid(ContainerType).name(),
+                        " for CLI::List argument.");
+
+                return_value.push_back(interm_value);
+        }
+
+        return return_value;
 }
+
+
+// specializations added to fix multi-word arguments
+
+inline List::operator std::vector<std::string>() const
+{
+        return value;
+}
+
+
+
+inline List::operator std::list<std::string>() const
+{
+        return std::list<std::string>(value.begin(), value.end());
+}
+
+
+
+inline List::operator std::vector<char*>() const
+{
+        std::vector<char*> return_value;
+        for (const auto& item: value)
+                return_value.push_back(const_cast<char*>(item.c_str()));
+
+        return return_value;
+}
+
+
+
+inline List::operator std::list<char*>() const
+{
+        std::list<char*> return_value;
+        for (const auto& item: value)
+                return_value.push_back(const_cast<char*>(item.c_str()));
+
+        return return_value;
+}
+
+
+
+inline List::operator std::vector<const char*>() const
+{
+        std::vector<const char*> return_value;
+        for (const auto& item: value)
+                return_value.push_back(const_cast<char*>(item.c_str()));
+
+        return return_value;
+}
+
+
+
+inline List::operator std::list<const char*>() const
+{
+        std::list<const char*> return_value;
+        for (const auto& item: value)
+                return_value.push_back(const_cast<char*>(item.c_str()));
+
+        return return_value;
+}
+
+
 
 
 
