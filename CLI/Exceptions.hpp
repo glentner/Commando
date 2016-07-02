232e50c09371f7d27430e10ba01ccdb3b7f8d2bd
Geoffrey Lentner <glentner@nd.edu>
2016-07-02 11:16:34 -0400

    Indentation

--- CLI/Exceptions.hpp
+++ CLI/Exceptions.hpp
@@ -23,49 +23,57 @@ namespace CLI {
 class Exception {
 public:
 
-        template<typename... Args>
-	explicit Exception(Args &&... args)
-        {
-                Init(std::forward<Args>(args)...);
-        }
+	template<typename... Args>
+	explicit Exception(Args &&... args) {
 
-        template<typename... Args>
-        void Init(Args &&... args)
-	{
+		Init(std::forward<Args>(args)...);
+	}
+
+	template<typename... Args>
+	void Init(Args &&... args) {
+
 		SSPrint(__stream, std::forward<Args>(args)...);
 	}
 
-        virtual const char* what() const throw(){ return __stream.str().c_str(); }
+	virtual const char* what() const throw(){ return __stream.str().c_str(); }
 
-        // FIXME: for g++-4.8 under Ubuntu somewhere it is complaining that we wanted
-        // to stream << arg0 with [arg0 = CLI::Exception]. I don't get it.
-        friend std::ostream& operator << (std::ostream& stream, const Exception& e)
-        {
-                return stream << e.what();
-        }
+	// FIXME: for GNU g++ under Ubuntu somewhere it is complaining that we wanted
+	// to stream << arg0 with [arg0 = CLI::Exception]. I don't get it.
+	friend std::ostream& operator << (std::ostream& stream, const Exception& e) {
 
-protected:
+		return stream << e.what();
+	}
 
-        std::stringstream __stream;
+	protected:
+
+	std::stringstream __stream;
 };
 
 class Error : public Exception {
 public:
 
 	template<typename... Args>
-	Error(Args &&... args):
-        Exception("Error: ", std::forward<Args>(args)...) {}
+	Error(Args &&... args): Exception("Error: ", std::forward<Args>(args)...) {}
 };
 
 class Usage: public Exception {
 public:
-        template<typename... Args>
-	Usage(Args &&... args):
-        Exception(std::forward<Args>(args)...) {}
+
+	template<typename... Args>
+	Usage(Args &&... args):	Exception(std::forward<Args>(args)...) {}
 };
 
 
+// object for setting CLI application to (non-catching mode)
+class Catch {
+public:
 
-} // namespace RootTree
+	Catch(bool except = true): except(except) {}
+	~Catch(){}
 
+	bool except;
+};
+
+
+} // namespace CLI
 #endif
